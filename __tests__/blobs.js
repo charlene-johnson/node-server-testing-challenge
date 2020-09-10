c2b0b3fec75d7dd2cf13d919987f7ef03f26c02a
charlene-johnson <charlene.j8234@gmail.com>
2020-09-10 06:25:51 -0400

    Finished testing, all tests are passing after using TDD, reached mvp

---
+++ __tests__/blobs.js
@@ -0,0 +1,74 @@
+const supertest = require("supertest")
+const server = require("../server")
+const db = require("../data/config")
+
+beforeEach(async () => {
+    // runs the seed before the run of each test to start fresh
+    await db.seed.run()
+})
+
+afterAll(async () => {
+    // closes the database connection so the test doesn't hang
+    await db.destroy()
+})
+
+describe("blobs integration tests", () => {
+    it("GET /api/blobs", async () => {
+        const res = await supertest(server).get("/api/blobs")
+        expect(res.statusCode).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body).toHaveLength(5)
+        expect(res.body[2].name).toBe("Blob Ross")
+    })
+    it("GET /api/blobs/:id", async () => {
+        const res = await supertest(server).get("/api/blobs/2")
+        expect(res.statusCode).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body.name).toBe("Aunty Blob")
+    })
+    it("GET /api/blobs/:id -Not found", async () => {
+        const res = await supertest(server).get("/api/blobs/30")
+        expect(res.statusCode).toBe(404)
+    })
+    it("POST /api/blobs", async () => {
+        const res = await supertest(server)
+            .post("/api/blobs")
+            .send({name: "Boss Blob"})
+        expect(res.statusCode).toBe(201)
+        expect(res.type).toBe("application/json")
+        expect(res.body.name).toBe("Boss Blob")
+    })
+    it("POST /api/blobs", async () => {
+        const res = await supertest(server)
+            .post("/api/blobs")
+            .send({name: "Baby Blob"})
+            expect(res.statusCode).toBe(201)
+            expect(res.type).toBe("application/json")
+            expect(res.body.name).toBe("Baby Blob")
+    })
+    it("PUT /api/blobs/:id", async () => {
+        const res = await supertest(server)
+        .put("/api/blobs/1")
+        .send({name: "Bobby Blob"})
+        expect(res.statusCode).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body.name).toBe("Bobby Blob")
+    })
+    it("PUT /api/blobs/:id", async () => {
+        const res = await supertest(server)
+        .put("/api/blobs/2")
+        .send({name: "Aunty Shirley Blob"})
+        expect(res.statusCode).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body.name).toBe("Aunty Shirley Blob")
+    })
+    it("DELETE /api/blobs/:id", async () => {
+        const res = await supertest(server)
+        .delete("/api/blobs/1")
+        expect(res.statusCode).toBe(204)
+    })
+    it("DELETE /api/blobs/:id -Not found", async () => {
+        const res = await supertest(server).get("/api/blobs/10")
+        expect(res.statusCode).toBe(404)
+    })
+})
--- blobs/blobs-router.js
+++ blobs/blobs-router.js
@@ -3,5 +3,57 @@ const Blobs = require("./blobs-model")
 
 const router = express.Router()
 
+router.get("/", async (req, res, next) => {
+    try {
+        res.json(await Blobs.find())
+    } catch(err) {
+        next(err)
+    }
+})
 
+router.get("/:id", async (req, res, next) => {
+    try {
+        const blob = await Blobs.findById(req.params.id)
+        if(!blob) {
+            return res.status(404).json({
+                message: "Blob not found",
+            })
+        }
+        res.json(blob)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const blob = await Blobs.create(req.body)
+        res.status(201).json(blob)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put("/:id", async (req, res, next) => {
+    try {
+        const blob = await Blobs.update(req.params.id, req.body)
+        res.status(200).json(blob)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.delete("/:id", (req, res, next) => {
+    try {
+        const blob = Blobs.remove(req.params.id)
+        if(!blob) {
+            return res.status(404).json({
+                message: "Blob not found"
+            })
+        }
+        res.status(204).end()
+    } catch(err) {
+        next(err)
+    }
+})
 module.exports = router
