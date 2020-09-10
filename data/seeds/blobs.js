bf95547935a8754ffd04bff77ffcbb1cad071e50
charlene-johnson <charlene.j8234@gmail.com>
2020-09-10 18:09:52 -0400

    Changed my blobs table, planning on doing some front end work to display blobs

--- data/seeds/blobs.js
+++ data/seeds/blobs.js
@@ -2,11 +2,11 @@
 exports.seed = async function(knex) {
  await knex("blobs").truncate()
  await knex("blobs").insert([
-   {name: "Blob Bob"},
-   {name: "Aunty Blob"},
-   {name: "Blob Ross"},
-   {name: "Professor Blob"},
-   {name: "Blob Marley"},
+   {name: "Blobby", picture:"https://imgur.com/zyaeNGg"},
+   {name: "Aunty Blob", picture:"https://imgur.com/6278CvN"},
+   {name: "Blob Ross", picture:"https://imgur.com/oq91Dj8"},
+   {name: "Professor Blob", picture:"https://imgur.com/bu1hF0j"},
+   {name: "Blob Marley", picture:"https://imgur.com/ua4qQeK"},
 
  ])
 };
